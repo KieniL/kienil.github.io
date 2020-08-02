57da94579c3283aa947ed355f5000085df7164d4
Lukas Kienast <lukas.kienast@nowconsultians.at>
2020-08-02 21:41:20 +0200

    Implemented fullpage scroll

--- js/swup.js
+++
@@ -1,9 +0,0 @@
-
-//Set the Link Selectors to allow other leading signs on href than # and /
-const options = {
-    linkSelector:
-      'a[href^="' +
-      window.location.origin +
-      '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup])'
-  };
-  const swup = new Swup(options)
